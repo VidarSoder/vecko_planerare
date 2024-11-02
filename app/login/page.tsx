2048a1cc450e0c2acd31721c4f71165ef8955840
videvid <vidar@videvit.se>
2024-11-02 23:08:03 +0100

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import { CleanSignin } from '@/components/clean-signin';
 
 export default function LoginPage() {
